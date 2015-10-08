50bc2ea6503fd0376cd49b74d788b38d8b4a1cf0
jobenscott <your.jobenscott@yahoo.com>
2015-10-07 22:04:28 -0700

    heroku commits

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def view
+  end
+
+  def add
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
